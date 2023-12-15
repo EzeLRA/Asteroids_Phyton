7157f17d9d18d6ed30e8903b2500f1de3f29185a
EzeLRA <ezeleocraft@gmail.com>
2023-12-14 23:12:20 -0300

    Delay Functions

--- textPrueba.py
+++ textPrueba.py
@@ -4,6 +4,59 @@ import pygame
 import math
 from functions import Rotate
 
+
+
+def delaySeconds(tS,aux,temp):
+    tiempoActual = int(pygame.time.get_ticks() / 1000)
+    cumple = False
+
+    if aux[0] == tiempoActual:
+        t = aux[0] - 1
+        print("Segundo Actual:",t)
+
+        if (tS[0] <= temp):
+            tS[2] = t
+
+        tS[0] = tS[2] - tS[1]
+        
+        if tS[0]>=temp:
+            print("Pasaron ",temp," segundos")
+            tS[0] = 0
+            tS[1] = tS[2]
+            cumple = True
+
+        aux[0] += 1
+
+    return cumple
+
+def delayMiliSeconds(tA,ml1,v,tmS,temp):#Aproximation to Miliseconds in base to Ticks
+    tiempoActual = pygame.time.get_ticks()
+    cumple = False
+
+    if tA[0] != 0:
+        if tiempoActual > tA[0]:
+            v[0] = (tiempoActual - tA[0])/1000
+            ml1[0] += v[0]
+            mili2 = int(ml1[0]*1000)
+            print(mili2)
+
+            if (tmS[0] <= temp):
+                tmS[2] = mili2
+
+            tmS[0] = tmS[2] - tmS[1]
+        
+            if tmS[0]>=temp:
+                print("Pasaron ",temp," milisegundos")
+                tmS[0] = 0
+                tmS[1] = tmS[2]
+                cumple = True
+
+    tA[0] = tiempoActual
+
+    return cumple        
+
+
+
 # Configuration
 pygame.init()
 fps = 60
@@ -18,25 +71,38 @@ pygame.key.set_repeat(10)
 
 running = True
 
+cambio = False
+
+#Time in Seconds:
+tSeconds = [0,0,0]
+temp = 1 #temporizador para contar segundos
+aux = [1]
+
+#Time in Miliseconds
+tmS = [0,0,0]
+tempMili = 100
+mili1 = [0]
+tiempoAnterior = [0]
+variacion=[0]
+
 # Game loop.
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill("black")
-
-    letra30 = pygame.font.SysFont("Arial", 30)                       
-    imagenTextoPresent = letra30.render('Texto',True, (200,200,200), (0,0,0))                               
-    rectanguloTextoPresent = imagenTextoPresent.get_rect()          
-    rectanguloTextoPresent.centerx = screen.get_rect().centerx
-    rectanguloTextoPresent.centery = screen.get_rect().centery  
-
-    texto = letra.render("Hola , Saludos",True,"White")
-    screen.blit(texto,(20,20))
-
-    screen.blit(imagenTextoPresent, rectanguloTextoPresent)
-
+    
+    """
+    if delaySeconds(tSeconds,aux,temp):
+        screen.fill("white")
+    else:
+        screen.fill("black")
+    """
+    
+    if delayMiliSeconds(tiempoAnterior,mili1,variacion,tmS,tempMili):
+        screen.fill("white")
+    else:
+        screen.fill("black")
+    
     pygame.display.flip()
     fpsClock.tick(fps)
 
